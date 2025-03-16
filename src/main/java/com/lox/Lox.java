f019909fc33f678e39be43fb633c8d656bcaf73b
kamlocicho <orwat.kamil3@gmail.com>
2025-03-16 13:19:43 +0100

    Reading files and tokens scanner

--- src/main/java/com/lox/Lox.java
+++ src/main/java/com/lox/Lox.java
@@ -1,22 +1,63 @@
 package com.lox;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Lox {
+    static boolean hadError = false;
+
     public static void main(String[] args) {
-        if (args.length > 1) {
-            System.out.println("Usage: jlox [script]");
-            System.exit(64);
-        } else if (args.length == 1) {
-            runFile(args[0]);
-        } else {
-            runPrompt();
+        try {
+            if (args.length > 1) {
+                System.out.println("Usage: Lox [script]");
+                System.exit(64);
+            } else if (args.length == 1) {
+                runFile(args[0]);
+            } else {
+                runPrompt();
+            }
+        } catch (IOException exception) {
+            throw new RuntimeException("Error reading from file / console.");
         }
     }
 
-    private static void runFile(String path) {
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
 
+        // Indicate an error in the exit code.
+        if (hadError) System.exit(65);
     }
-    
-    private static void runPrompt() {
 
+    private static void runPrompt() throws IOException {
+        var input = new InputStreamReader(System.in);
+        var reader = new BufferedReader(input);
+        while (true) {
+            System.out.print("> ");
+            var line = reader.readLine();
+            if (line.isBlank()) break;
+            run(line);
+            hadError = false;
+        }
+    }
+
+    private static void run(String source) {
+        var scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+        tokens.forEach(System.out::println);
+    }
+
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+    private static void report(int line, String where, String message) {
+        System.err.printf("[line %s] Error %s: %s%n", line, where, message);
+        hadError = true;
     }
 }
--- src/main/java/com/lox/TokenType.java
+++ src/main/java/com/lox/TokenType.java
@@ -1,2 +1,22 @@
-package com.lox;public enum TokenType {
+package com.lox;
+
+public enum TokenType {
+    // SINGLE CHARACTERS
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    // ONE OR TWO CHARACTERS
+    BANG, BANG_EQUAL,
+    EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
+    LESS, LESS_EQUAL,
+
+    // LITERALS
+    IDENTIFIER, STRING, NUMBER,
+
+    // KEYWORDS
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    EOF
 }
